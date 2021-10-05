bbc2969d7e6ba6527e0fa7932b36132b53460052
Anthony José <anthonyJ2017@icloud.com>
2021-10-05 08:54:49 -0300

    Initial Commit

---
+++ Shared/CoreDataBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBootcampApp.swift
+//  Shared
+//
+//  Created by Anthony José on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
